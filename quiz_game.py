867c212ccc6e0df4efb3f4092a3c55fa24b5bc76
PKILB <paytonboone30@gmail.com>
2023-05-24 12:47:19 -0600

    can ask the user questions

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,12 @@
 print("Welcome to my League Of Legends Quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+
+answer = input("Who is the hardest ADC? ")
+if answer == "Aphelios":
+    print("Correct!")
