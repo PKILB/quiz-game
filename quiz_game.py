e1ad8385c4ef66c3d30fee908320d41031166ba0
PKILB <paytonboone30@gmail.com>
2023-05-24 13:08:34 -0600

    finished simple python beginners quiz game

--- quiz_game.py
+++ quiz_game.py
@@ -6,27 +6,35 @@ if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play :)")
+score = 0
 
 answer = input("Who is the hardest ADC? ")
-if answer.lower() == "Aphelios":
+if answer.lower() == "aphelios":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect......")
 
 answer = input("Who is the most cringe Top Laner? ")
-if answer.lower() == "Teemo":
+if answer.lower() == "teemo":
     print("Correct!")
+    score += 1 
 else:
     print("Incorrect......")
 
 answer = input("Who is the coolest Mid Laner? ")
-if answer.lower() == "Sylas":
+if answer.lower() == "sylas":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect......")
 
 answer = input("Who is the scariest Jungler? ")
-if answer.lower() == "Eveylnn":
+if answer.lower() == "eveylnn":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect......")
+
+    print("You got " + str(score) + " questions correct!")
+    print("You got " + str((score/4) * 100) + "%.")
