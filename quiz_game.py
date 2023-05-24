45faeecf075d916c31663418a22552197f56ea8e
PKILB <paytonboone30@gmail.com>
2023-05-24 12:52:32 -0600

    more questions created

--- quiz_game.py
+++ quiz_game.py
@@ -10,3 +10,23 @@ print("Okay! Let's play :)")
 answer = input("Who is the hardest ADC? ")
 if answer == "Aphelios":
     print("Correct!")
+else:
+    print("Incorrect......")
+
+answer = input("Who is the most cringe Top Laner? ")
+if answer == "Teemo":
+    print("Correct!")
+else:
+    print("Incorrect......")
+
+answer = input("Who is the coolest Mid Laner? ")
+if answer == "Sylas":
+    print("Correct!")
+else:
+    print("Incorrect......")
+
+answer = input("Who is the scariest Jungler? ")
+if answer == "Eveylnn":
+    print("Correct!")
+else:
+    print("Incorrect......")
